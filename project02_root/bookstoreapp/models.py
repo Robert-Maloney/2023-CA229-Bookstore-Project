62840a917ce992a5d92b18d319cf304a4c732379
Robert Maloney <robert.maloney26@mail.dcu.ie>
2023-04-10 18:15:27 +0100

    Updated website

--- project02_root/bookstoreapp/models.py
+++ project02_root/bookstoreapp/models.py
@@ -10,3 +10,23 @@ class Book(models.Model):
     release_year = models.DecimalField(max_digits=4, decimal_places=0)
     price = models.DecimalField(max_digits=5, decimal_places=2) # number from 0.0-999.99
     synopsis = models.TextField()
+    ADVANCED = "Advanced"
+    BEGINNER = "Beginner"
+    WEIGHTLOSS = "Weight-loss"
+    VEGETARIAN = "Vegetarian"
+    REGIONAL = "Regional"
+    DRINKS = "Drinks"
+    BAKING = "Baking"
+    CATEGORY_CHOICES = [
+        (ADVANCED, "Advanced"),
+        (BEGINNER, "Beginner"),
+        (WEIGHTLOSS, "Weight-loss"),
+        (VEGETARIAN, "Vegetarian"),
+        (REGIONAL, "Regional"),
+        (DRINKS, "Drinks"),
+        (BAKING, "Baking"),
+        ]
+    category = models.CharField(max_length=15, choices=CATEGORY_CHOICES, default=BEGINNER,)
+
+    def __str__(self):
+        return self.title
--- project02_root/bookstoreapp/views.py
+++ project02_root/bookstoreapp/views.py
@@ -12,6 +12,13 @@ def single_book(request, id):
     book = get_object_or_404(Book, id=id)
     return render(request, 'single_book.html', {'book': book})
 
+def books_by_category(request, book_category):
+    allbooks = Book.objects.filter(category=book_category)
+    return render(request, 'all_books.html', {'allbooks': allbooks})
+
+def category(request):
+    category_set = set([x.category for x in Book.objects.all()])
+    return render(request, 'all_books.html', {'categories': category_set})
 
 def index(request):
     return render(request, 'index.html')
